76f3a42d0fb42a7ab5c35628b64b116f3804aa50
3bkreemm <abdelkarimmohsen28@gmail.com>
2025-08-27 19:56:53 +0300

    Route.1

---
+++ app/Http/Controllers/TestController.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Http\Controllers;
+
+class TestController extends Controller
+{
+    public function abdo(){
+        dd('hello abdo from TestController file');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,16 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\TestController;
+
+//to return view
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('index');
 });
+
+// Route::get('/3bkreem',function (){
+//     return "I'm abdlekarim mohsen";
+// });
+
+Route::get('/abdo',[TestController::class,'abdo']);
